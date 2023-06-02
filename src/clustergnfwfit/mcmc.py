b60bd136e164457ac32bde7d1bc621602a35d1e5
Harry Lu <realharrylu@gmail.com>
2023-06-02 14:23:04 -0700

    Updated priors for P0.

--- src/clustergnfwfit/mcmc.py
+++ src/clustergnfwfit/mcmc.py
@@ -83,7 +83,7 @@ print(f'theta bounds: [{theta_lower, theta_upper}]')
 def log_prior(p):
     theta, p0_90, p0_150, r_x, r_y, offset_x, offset_y, c_90, c_150 = p
     # bound r_x, r_y > 0
-    in_bounds = theta_lower < theta < theta_upper and -2000 < p0_90 < 2000 and -2000 < p0_150 < 2000 and 10 < r_x < 1000 and 10 < r_y < r_x
+    in_bounds = theta_lower < theta < theta_upper and -5000 < p0_90 < 5000 and -5000 < p0_150 < 5000 and 10 < r_x < 1000 and 10 < r_y < r_x
     in_bounds &= -100 < offset_x < 100 and -100 < offset_y < 100
     if in_bounds:
         return 0
